1b1403e5704ce8b919e6113316fe6ea5f4ea82c6
JCarosella <juancarosella96@creativa77.com.ar>
2023-11-29 12:57:31 -0300

    Adding convenience script to launch omniverse

---
+++ run_omniverse.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/aeneas/.local/share/ov/pkg/isaac_sim-2023.1.0/exts/omni.isaac.ros2_bridge/humble/lib
+
+
+/home/aeneas/.local/share/ov/pkg/isaac_sim-2023.1.0/isaac-sim.sh
+
