ec84b5acf109396dd3bd2cb9ab8875053f8dc23c
JCarosella <juancarosella96@creativa77.com.ar>
2023-12-01 13:02:45 -0300

    Reworking whole launch script

--- isaac_launch_script.py
+++ isaac_launch_script.py
@@ -1,63 +1,64 @@
+import argparse
+import numpy as np
 import os
 import carb
-from omni.isaac.core.utils.extensions import enable_extension
-from omni.isaac.core.utils.stage import open_stage
-from omni.isaac.core.utils.stage import add_reference_to_stage
+from omni.isaac.kit import SimulationApp
+
+# This sample loads a usd stage and starts simulation
+CONFIG = {"width": 1280, "height": 720, "sync_loads": True, "headless": False, "renderer": "RayTracedLighting"}
+
+# Set up command line arguments
+parser = argparse.ArgumentParser("Simulation loader argument parser")
+parser.add_argument("--world_file", default="/home/aeneas/omniverse/andino_isaac/isaac_worlds/plain_world.usda", help="Full path to the world file")
+parser.add_argument("--robot_file", default="/home/aeneas/omniverse/andino_isaac/andino_isaac_description/just_andino.usda", help="Full path to the robot file")
+args, unknown = parser.parse_known_args()
+
+# Start the omniverse application
+simulation_app = SimulationApp(launch_config=CONFIG)
+
+# Now that the simulation app is open, continue to load the extension, world and robot
+# These imports need to be here as they depend on SimulationApp
 from omni.isaac.core import World
-from omni.isaac.version import get_version
+from omni.isaac.core.utils.extensions import enable_extension
+from omni.isaac.core.utils.stage import open_stage, is_stage_loading, add_reference_to_stage
 
-class SimulationLoader(object):
-	def __init__(self) -> None:
-		self._world = None
-		self._current_tasks = None
-		self._world_settings = {"physics_dt": 1.0 / 60.0, "stage_units_in_meters": 1.0, "rendering_dt": 1.0 / 60.0}
-		return
+# Enable the ros2_bridge extension. Environment variables must be set
+if not enable_extension("omni.isaac.ros2_bridge"):
+	carb.log_error("ROS2_BRIDGE extension could not be loaded, aborting startup")
+	simulation_app.close()
 
-	def load(self):
-		# ROS2_BRIDGE extension is critical
-		if not self.enable_ros2_bridge_extension():
-			print ("Unable to load ros2_bridge extension, aborting startup")
-			return
-		# World is critical
-		if not self.load_world():
-			print ("Unable to load world, aborting startup")
-			return
-		# Robot is not critical, do not abort if loading fails
-		self.spawn_robot()
-		#self._world.play()
+# Open world
+try:
+	carb.log_info("Loading world please wait")
+	open_stage(args.world_file)
+	# Wait two frames so that stage starts loading
+	simulation_app.update()
+	simulation_app.update()
+	while is_stage_loading():
+			simulation_app.update()
+	world_settings = {"physics_dt": 1.0 / 60.0, "stage_units_in_meters": 1.0, "rendering_dt": 1.0 / 60.0}
+	world = World(**world_settings)
+	carb.log_info("World loaded")
+except ValueError:
+	carb.log_error("Stage could not be loaded, check file path. Aborting startup")
+	simulation_app.close()
 
-	def enable_ros2_bridge_extension(self) -> bool:
-		"""
-		Method to make sure the ROS2_BRIDGE Isaac extension is enabled, aborting execution if it's not
-		"""
-		# Enable ROS2_bridge extension. This must be done before loading Andino given that the action graphs depend on the extension
-		if not enable_extension("omni.isaac.ros2_bridge"):
-			print("ROS2_BRIDGE extension could not be loaded, aborting startup")
-			return False
-		return True
+# Load robot
+print("Loading robot")
+try:
+	add_reference_to_stage(usd_path=args.robot_file, prim_path="/andino")
+	carb.log_info("Robot loaded")
+except FileNotFoundError:
+	carb.log_warn("Robot could not be loaded. Check robot file path or load it manually in the simulation")
 
-	def load_world(self) -> bool:
-		"""
-		Method to load the specified simulation world
-		"""
-		try:
-			open_stage("/home/aeneas/omniverse/andino_isaac/isaac_worlds/plain_world.usda")
-			self._world = World(**self._world_settings)
-		except ValueError:
-			print("Stage could not be loaded, check file path")
-			return False
-		return True
+world.reset()
 
-	def spawn_robot(self):
-		"""
-		Method to spawn the robot as a reference
-		"""
-		try:
-			add_reference_to_stage(usd_path="/home/aeneas/omniverse/andino_isaac/andino_isaac_description/andino_isaac.usda", prim_path="/andino")
-		except FileNotFoundError:
-			print("Robot could not be loaded. Check robot file path or load it manually in the simulation")
-		return
+while simulation_app.is_running():
+    world.step()
 
-if __name__ == '__main__':
-	sim_loader = SimulationLoader()
-	sim_loader.load()
+    # Deal with pause/stop
+    if world.is_playing():
+        if world.current_time_step_index == 0:
+            world.reset()
+
+simulation_app.close()
