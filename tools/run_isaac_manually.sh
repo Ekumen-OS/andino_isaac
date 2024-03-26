c6220c81f94f9a5d41bec8ed041f7e242e906843
Franco Cipollone <franco.c@ekumenlabs.com>
2024-03-26 01:45:19 -0300

    Makes script for manually running issac generic
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

--- tools/run_isaac_manually.sh
+++ tools/run_isaac_manually.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/aeneas/.local/share/ov/pkg/isaac_sim-2023.1.0/exts/omni.isaac.ros2_bridge/humble/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/$(whoami)/.local/share/ov/pkg/isaac_sim-2023.1.1/exts/omni.isaac.ros2_bridge/humble/lib
 
 
-/home/aeneas/.local/share/ov/pkg/isaac_sim-2023.1.0/isaac-sim.sh
+/home/$(whoami)/.local/share/ov/pkg/isaac_sim-2023.1.1/isaac-sim.sh
 
