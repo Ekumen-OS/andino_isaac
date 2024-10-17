65ff7e798e55feeeffe269b6a49e85d1c7e37664
JuanFSStudio <juan.carosella@fsstudio.com>
2024-10-17 13:17:00 -0300

    Bumping to 4.2, simplifying docker and adding compose

---
+++ docker/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo chown dev ~/.isaac-sim-cache -R
+bash
--- tools/run_isaac_manually.sh
+++ tools/run_isaac_manually.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/$(whoami)/.local/share/ov/pkg/isaac-sim-4.0.0/exts/omni.isaac.ros2_bridge/humble/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/$(whoami)/.local/share/ov/pkg/isaac-sim-4.2.0/exts/omni.isaac.ros2_bridge/humble/lib
 
 
-/home/$(whoami)/.local/share/ov/pkg/isaac-sim-4.0.0/isaac-sim.sh
+/home/$(whoami)/.local/share/ov/pkg/isaac-sim-4.2.0/isaac-sim.sh
 
