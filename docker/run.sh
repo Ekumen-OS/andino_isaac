15b349a90272112bacbba546e7a2cd7b4f21300f
JCarosella <juancarosella96@creativa77.com.ar>
2025-10-27 13:40:22 -0300

    Updating to 5.1
    Signed-off-by: JCarosella <juancarosella96@creativa77.com.ar>

--- docker/run.sh
+++ docker/run.sh
@@ -16,4 +16,18 @@ if [[ ! -z "$1" ]]; then
     fi
 fi
 
-docker compose run ${BUILD} --rm andino_isaac --remove-orphans
+# Create cache directories in host machine and give container user permissions
+echo "Creating Isaac sim cache directories"
+mkdir -p ~/docker/isaac-sim-andino/cache/main/ov
+mkdir -p ~/docker/isaac-sim-andino/cache/main/warp
+mkdir -p ~/docker/isaac-sim-andino/cache/computecache
+mkdir -p ~/docker/isaac-sim-andino/config
+mkdir -p ~/docker/isaac-sim-andino/data/documents
+mkdir -p ~/docker/isaac-sim-andino/data/Kit
+mkdir -p ~/docker/isaac-sim-andino/logs
+mkdir -p ~/docker/isaac-sim-andino/pkg
+sudo chown -R 1234:1234 ~/docker/isaac-sim-andino
+
+LOCAL_UID=$(id -u) LOCAL_GID=$(id -g) docker compose run ${BUILD} --remove-orphans --rm andino_isaac
+
+sudo xhost -
