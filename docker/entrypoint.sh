1079fa697d5fa7b9e8ac152864be4e3a07109040
Marcos Wagner <wagnermarcos@creativa77.com.ar>
2025-11-27 15:24:05 -0500

    Remove paths that were removed for 5.1.0
    Signed-off-by: Marcos Wagner <wagnermarcos@creativa77.com.ar>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-
-sudo chown "$USER" /isaac-sim/kit/cache -R
-sudo chown "$USER" /isaac-sim/kit/data -R
-sudo chown "$USER" /isaac-sim/kit/logs -R
 source /opt/ros/humble/setup.bash
 bash
